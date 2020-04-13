7407306ae9acc563f4ba069735d0dd96b9f43129
Ouria-dev <55099785+Ouria-dev@users.noreply.github.com>
2020-04-13 14:16:48 +0200

    Création Class PostManager et MembersManager

--- writerBlog/model/MembersManager.php
+++ writerBlog/model/MembersManager.php
@@ -1,1 +1,37 @@
 <?php
+require_once("model/Manager.php");
+
+class MembersManager extends Manager
+{
+
+/*récupère les information relative à la connexion de l'user dans la bdd*/
+	public function getConnect($pseudo)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT id, motdepasse, droits FROM users WHERE pseudo = :pseudo');
+		$req->execute(array('pseudo' =>$pseudo));
+		$connect = $req->fetch();
+		return $connect;
+	}
+
+/*insertion nouveau membre dans la bdd*/	
+	public function insertMemberBdd($pseudo, $mail, $mdp)
+	{
+		$db = $this->dbConnect();
+		$insertmbr = $db->prepare("INSERT INTO users(pseudo, mail, motdepasse, droits) VALUES(?, ?, ?, 0)");
+        $insertmbr->execute(array($pseudo, $mail, $mdp));
+        return $insertmbr;
+
+	}
+
+/*test pour doublon email dans la Bdd*/	
+	public function emailtestBdd($mail)
+	{
+		$db = $this->dbConnect();
+		 $req = $db->prepare("SELECT * FROM users WHERE mail = ?");
+        $req->execute(array($mail));
+        $existEmail = $req->rowCount();
+        return $existEmail;
+	}
+
+}
--- writerBlog/model/PostManager.php
+++ writerBlog/model/PostManager.php
@@ -1,1 +1,79 @@
-<?php ?>
+<?php
+require_once("model/Manager.php");
+
+class PostManager extends Manager // héritage class Manager
+{
+
+/* Ajoute chapitre à la BDD */
+	public function addPostBdd($title, $content)
+	{
+		$db = $this->dbConnect();
+		$insertPost = $db->prepare('INSERT INTO posts(title, content, creation_date) VALUES (?, ?, NOW())');
+        $chapitre = $insertPost->execute(array($title, $content));
+		
+		return $chapitre;
+	}
+
+/* Modif chapitre dans la BDD */
+	public function updatePostBdd($title, $content, $postId)
+	{
+		$db = $this->dbConnect();
+		$editPost = $db->prepare('UPDATE posts SET title = ?, content = ? WHERE id = ?');
+		$chapitreEdit = $editPost->execute(array($title, $content, $postId));
+		
+		return $chapitreEdit;
+	}
+/* Supprime un chapitre et ses commentaires dans la BDD */
+	public function delPostBdd($dataId)
+	{ 
+        $db = $this->dbConnect();
+        $comment = $db->prepare('DELETE FROM comments WHERE id_billet = ?');
+        $comment->execute([$dataId]);
+        $req = $db->prepare('DELETE FROM posts WHERE id = ?');
+		$req->execute(array($dataId));
+		
+       	return $req;
+	}
+
+/* Récupération de tous les chapitres par ordre décroissant "users" */	
+	public function getPostsBdd()
+	{
+		$db = $this->dbConnect();
+		$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 30');
+
+		return $req;
+	}
+
+/* Récupération chapitre par id */
+	public function getPostBdd($postId)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+		$req->execute(array($postId));
+		$post = $req->fetch();
+
+		return $post;
+
+	}
+
+/* Récupération de tous les chapitres par ordre décroissant "Admin" */
+	public function getPostsAdminBdd()
+	{
+		$db = $this->dbConnect();
+		$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 10');
+
+		return $req;
+	}
+
+
+/* Récupération chapitre par id "Admin" */
+    public function getPostIdBdd($postId)
+    {
+    	$db = $this->dbConnect();
+    	$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+		$req->execute(array($postId));
+	
+    	return $req;
+	}
+
+}
