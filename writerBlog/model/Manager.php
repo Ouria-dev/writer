29f60825654988dd16e8f4fdbee545a7cd2f809a
Ouria-dev <55099785+Ouria-dev@users.noreply.github.com>
2020-04-13 14:16:48 +0200

    Création Class Manager

--- writerBlog/model/Manager.php
+++ writerBlog/model/Manager.php
@@ -1,1 +1,16 @@
 <?php
+class Manager
+{
+	protected function dbConnect()// méthode de connexion à la bdd
+	{
+		try
+		{
+			$db = new PDO('mysql:host=localhost;dbname=pj4openclass;charset=utf8', 'root', '');
+			return $db;
+		}
+		catch (Exception $e)
+		{
+			die('Erreur : ' .$e->getMessage());
+		}
+	}
+}
