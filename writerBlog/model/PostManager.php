a9cfa93bbf2d15fb9b20fe6646df7fb1e52a08c5
Ouria-dev <55099785+Ouria-dev@users.noreply.github.com>
2020-05-05 16:23:04 +0200

    Modif postmanager ajout fonct prepare

--- writerBlog/model/PostManager.php
+++ writerBlog/model/PostManager.php
@@ -5,13 +5,13 @@ class PostManager extends Manager {
 
 
 /* Ajoute chapitre à la BDD */
-	public function addPostBdd($title, $content) {
-		$db = $this->dbConnect();
-		$insertPost = $db->prepare('INSERT INTO posts(title, content, creation_date) VALUES (?, ?, NOW())');
-        $chapitre = $insertPost->execute(array($title, $content));
-		
-		return $chapitre;
-	}
+public function addPostBdd($title, $content) {
+	$db = $this->dbConnect();
+	$insertPost = $db->prepare('INSERT INTO posts(title, content, creation_date) VALUES (?, ?, NOW())');
+	$chapitre = $insertPost->execute(array($title, $content));
+	
+	return $chapitre;
+}
 
 /* Modif chapitre dans la BDD */
 	public function updatePostBdd($title, $content, $postId) {
@@ -36,8 +36,8 @@ class PostManager extends Manager {
 /* Récupération de tous les chapitres par ordre décroissant "users" */	
 	public function getPostsBdd() {
 		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 30');
-
+		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 30');
+		$req->execute(array());
 		return $req;
 	}
 
@@ -54,8 +54,8 @@ class PostManager extends Manager {
 /* Récupération de tous les chapitres par ordre décroissant "Admin" */
 	public function getPostsAdminBdd() {
 		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 10');
-
+		$req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 10');
+		$req->execute(array());
 		return $req;
 	}
 
